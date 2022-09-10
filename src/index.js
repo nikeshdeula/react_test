cb8f09a2bd76b8a6fcf2c815a03b3c0cc3606440
nikeshDeula <nikeshdeula.21@gmail.com>
2022-09-10 17:16:27 +0545

    image showing

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ const fullName = "Random Generated Image";
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/202/300";
 const img3 = "https://picsum.photos/201/300";
+const link = "https://nickwalkerior.blogspot.com/";
 
 ReactDOM.render(
     <>
 <h1>Name: {fullName}</h1>;
-<img src={img1} alt='ramdom images'/>
-    <a href='Flamingo Tech'>
+<img src={img1} alt='ramdom images' />
+    <a href={link} target="_blank">
         <img src= {img2}/>
     </a>
-    <a href='Flamingo Tech'>
+    <a href={link} target="_blank">
         <img src= {img3}/>
     </a>
 
