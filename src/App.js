89fd49355a084bc80a87a42b9124ec4efae958f8
nikeshDeula <nikeshdeula.21@gmail.com>
2022-09-14 11:52:59 +0545

    calling image with js function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Heading from "./Heading";
+import Content from "./Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Heading />
+      <Content />
+    </>
   );
 }
 
 export default App;
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function App(){
+
+// let currDate = new Date();
+// currDate = currDate.getHours();
+// let greeting = "";
+// let cssstyle = {};
+
+// if(currDate >= 8 || currDate <12){
+//     greeting = "good Morning";
+//     cssstyle.color = "green";
+// }
+// else if(currDate >= 12 || currDate < 5){
+//     greeting = "good Evening";
+//     cssstyle.color = "pink";
+// }
+// else {
+//     greeting = "good Night";
+//     cssstyle.color = "red";
+// }
+
+// return (
+// <>
+//   <div>
+//   <h1>hello sir/madam <span style={cssstyle}> {greeting} </span></h1>
+//   </div>
+//   </>)
+
+// };
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
-let currDate = new Date();
-currDate = currDate.getHours();
-let greeting = "";
-let cssstyle = {};
-
-
-if(currDate >= 8 || currDate <12){
-    greeting = "good Morning";
-    cssstyle.color = "green";
-}
-else if(currDate >= 12 || currDate < 5){
-    greeting = "good Evening";
-    cssstyle.color = "pink";
-}
-else {
-    greeting = "good Night";
-    cssstyle.color = "red";
-}
-
+import App from "./App";
 
 
 
 ReactDOM.render(
-<>
-<div>
-<h1>hello sir/madam <span style={cssstyle}> {greeting} </span></h1>
-</div>
-</>,
+<App/>,
+
 document.getElementById("root")
 );
 
