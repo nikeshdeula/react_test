8b4169559149f253b85e7ee7321ecf93b1a62d19
nikeshDeula <nikeshdeula.21@gmail.com>
2022-09-14 11:56:45 +0545

    calling image using js function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Heading from "./Heading";
 import Content from "./Content";
 
+//Calling imaging using js functions
+
 function App() {
   return (
     <>
@@ -13,38 +15,3 @@ function App() {
 
 export default App;
 
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// function App(){
-
-// let currDate = new Date();
-// currDate = currDate.getHours();
-// let greeting = "";
-// let cssstyle = {};
-
-// if(currDate >= 8 || currDate <12){
-//     greeting = "good Morning";
-//     cssstyle.color = "green";
-// }
-// else if(currDate >= 12 || currDate < 5){
-//     greeting = "good Evening";
-//     cssstyle.color = "pink";
-// }
-// else {
-//     greeting = "good Night";
-//     cssstyle.color = "red";
-// }
-
-// return (
-// <>
-//   <div>
-//   <h1>hello sir/madam <span style={cssstyle}> {greeting} </span></h1>
-//   </div>
-//   </>)
-
-// };
-
-// export default App;
