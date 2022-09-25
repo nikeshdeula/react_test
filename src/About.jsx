872307da9c9711277b725450fca3c3258600b7ae
nikeshDeula <nikeshdeula.21@gmail.com>
2022-09-26 00:53:07 +0545

    router

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from './Components/Header';
+
 
 
 const About = () => {
 return (
    <>
-   <Header />
    <h1>i am About Page</h1>
    </>
 );
--- src/MainHeader.jsx
+++ src/MainHeader.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header';
+
 
 const MainHeader = () => {
     return (
         <>
+        <Header />
+        <Outlet />
         </>
     );
 }
