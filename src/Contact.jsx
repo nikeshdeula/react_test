8c769a12f2429f07d49421ce29e592c9b10b932f
nikeshDeula <nikeshdeula.21@gmail.com>
2022-09-25 23:52:53 +0545

    react router

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -14,3 +14,26 @@ const Contact = () => {
 }
 
 export default Contact;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void push (int stack[ ] , int x , int n) {
+//     if ( top == n-1 ) {         //If the top position is the last of position in a stack, this means that the stack is full
+//        cout << “Stack is full.Overflow condition!” ;
+//        }
+//        else{
+//            top = top +1 ;            //Incrementing top position 
+//            stack[ top ] = x ;       //Inserting element on incremented position  
+//        }
+//    }
